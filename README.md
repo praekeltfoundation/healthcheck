1ec06d61e03a6e13a671ad3c890ee0f763750403
Rudi Giesler <rudi@praekelt.org>
2020-05-15 14:30:54 +0200

    Require postgresql

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 $ pip install -e .
 $ python manage.py test
 ```
+
+## Running locally
+```
+$ createdb healthcheck
+$ DJANGO_SETTINGS_MODULE=healthcheck.settings.text python manage.py migrate
+$ DJANGO_SETTINGS_MODULE=healthcheck.settings.text python manage.py runserver
--- healthcheck/settings/test.py
+++ healthcheck/settings/test.py
@@ -3,5 +3,5 @@ from healthcheck.settings.base import *  # noqa: F403
 SECRET_KEY = "testsecretkey"
 DEBUG = True
 DATABASES = {
-    "default": env.db(default="sqlite:///"),  # noqa: F405
+    "default": env.db(default="postgresql://localhost/healthcheck")  # noqa: F405
 }
