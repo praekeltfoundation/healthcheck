5f3bb5448b16c6ae324ad0f0446566c339e71768
Rudi Giesler <rudi@praekelt.org>
2020-05-15 17:03:41 +0200

    Add CSV import

--- confirmed_contact/admin.py
+++ confirmed_contact/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.core.paginator import Paginator
 from django.db import OperationalError, connection, transaction
+from django import forms
+from django.urls import path
+from django.template.response import TemplateResponse
+from django.shortcuts import redirect
 from django.utils.functional import cached_property
+import csv
 
 from confirmed_contact.models import ConfirmedContact
 
@@ -31,6 +36,10 @@ class ApproximatePaginator(Paginator):
             return int(cursor.fetchone()[0])
 
 
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+
+
 @admin.register(ConfirmedContact)
 class ConfirmedContactAdmin(admin.ModelAdmin):
     sortable_by = "msisdn"
@@ -39,6 +48,37 @@ class ConfirmedContactAdmin(admin.ModelAdmin):
     list_display = ("msisdn", "timestamp")
     search_fields = ("msisdn",)
     readonly_fields = ("created_by", "created_at")
+    change_list_template = "confirmed_contact_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [path("import_csv/", self.import_csv)]
+        return my_urls + urls
+
+    @transaction.atomic
+    def _process_csv(self, request, f):
+        decoded = f.read().decode("utf-8").splitlines()
+        reader = csv.DictReader(decoded)
+        objects = []
+        for row in reader:
+            cc = ConfirmedContact(**row)
+            cc.created_by = request.user.username
+            objects.append(cc)
+        ConfirmedContact.objects.bulk_create(objects, ignore_conflicts=True)
+        self.message_user(request, "CSV import successful")
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            form = CsvImportForm(request.POST, request.FILES)
+            if form.is_valid():
+                # If there are errors in the CSV, fail and report to sentry. This
+                # import is for internal use only
+                self._process_csv(request, request.FILES["csv_file"])
+                return redirect("..")
+        else:
+            form = CsvImportForm()
+        context = dict(self.admin_site.each_context(request), form=form)
+        return TemplateResponse(request, "csv_form.html", context)
 
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user.username
---
+++ confirmed_contact/test_admin.py
@@ -0,0 +1,48 @@
+import io
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.test import TestCase
+from confirmed_contact.models import ConfirmedContact
+from django.utils.timezone import utc
+
+
+class CsvImportTests(TestCase):
+    def test_csv_import(self):
+        """
+        Importing a CSV should insert the objects into the database, with the username
+        of the user who performed the import
+        """
+        User.objects.create_superuser("testuser", "test@example.org", "testpass")
+        self.client.login(username="testuser", password="testpass")
+        r = self.client.post(
+            "/admin/confirmed_contact/confirmedcontact/import_csv/",
+            {
+                "csv_file": io.StringIO(
+                    "\n".join(
+                        [
+                            "msisdn,timestamp",
+                            "+27820001003,2020-05-15 15:17:57.803857Z",
+                            "+27820001004,2020-05-15 15:17:58.803857Z",
+                        ]
+                    )
+                )
+            },
+            follow=True,
+        )
+
+        [message] = r.context["messages"]
+        self.assertEqual(message.message, "CSV import successful")
+
+        [c1, c2] = ConfirmedContact.objects.order_by("msisdn").all()
+
+        self.assertEqual(c1.msisdn, "+27820001003")
+        self.assertEqual(
+            c1.timestamp, datetime(2020, 5, 15, 15, 17, 57, 803857, tzinfo=utc)
+        )
+        self.assertEqual(c1.created_by, "testuser")
+
+        self.assertEqual(c2.msisdn, "+27820001004")
+        self.assertEqual(
+            c2.timestamp, datetime(2020, 5, 15, 15, 17, 58, 803857, tzinfo=utc)
+        )
+        self.assertEqual(c2.created_by, "testuser")
