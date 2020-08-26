e2f0c5d64f5a6d9da478f9fb18b928b215f13cd8
cnsr <dmytrenkoyar@gmail.com>
2020-08-26 15:10:39 +0300

    Changed contact field in case admin to be in raw_id_fields; adjusted width

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,10 +1,20 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Case, Contact
 
 
+class CaseEditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["contact"].widget.attrs.update({"style": "width: 260px"})
+
+    fields = "__all__"
+
+
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
+    form = CaseEditForm
     list_display = (
         "id",
         "external_id",
@@ -19,6 +29,7 @@ class CaseAdmin(admin.ModelAdmin):
         "created_at",
         "is_active",
     )
+    raw_id_fields = ("contact",)
 
 
 @admin.register(Contact)
