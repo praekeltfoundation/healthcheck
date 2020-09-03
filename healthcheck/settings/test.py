39878909db0bdde833581b750af3f46fdac499be
Erik Harding <erik@praekelt.org>
2020-09-03 12:43:16 +0200

    fix db name

--- healthcheck/settings/test.py
+++ healthcheck/settings/test.py
@@ -4,7 +4,7 @@ SECRET_KEY = "testsecretkey"
 DEBUG = True
 DATABASES = {
     "default": env.db(  # noqa: F405
-        default="postgres://postgres@localhost:5432/higher_health"
+        default="postgres://postgres@localhost:5432/healthcheck"
     ),
 }
 
