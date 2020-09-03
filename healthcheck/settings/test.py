4151625a7516b70c9d7e1793315dbaa8992afcee
Erik Harding <erik@praekelt.org>
2020-09-03 12:42:25 +0200

    add default db string

--- healthcheck/settings/test.py
+++ healthcheck/settings/test.py
@@ -3,7 +3,9 @@ from healthcheck.settings.base import *  # noqa: F403
 SECRET_KEY = "testsecretkey"
 DEBUG = True
 DATABASES = {
-    "default": env.db(),  # noqa: F405
+    "default": env.db(  # noqa: F405
+        default="postgres://postgres@localhost:5432/higher_health"
+    ),
 }
 
 CELERY_ALWAYS_EAGER = True
