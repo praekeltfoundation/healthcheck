ddad4d3e893ec7354cf67ffa73f5ba9d35652c27
OMosimege <onalerona.mosimege@gmail.com>
2020-10-22 14:10:59 +0200

    added additonal tests to check API calls

--- selfswab/tasks.py
+++ selfswab/tasks.py
@@ -1,6 +1,7 @@
 from celery.decorators import periodic_task
 from celery.task.schedules import crontab
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 
 from django_redis import get_redis_connection
 from temba_client.v2 import TembaClient
@@ -24,28 +25,36 @@ def poll_meditech_api_for_results():
                 settings.RAPIDPRO_URL, settings.SELFSWAB_RAPIDPRO_TOKEN
             )
 
-    barcodes = SelfSwabTest.objects.filter(
-        result=SelfSwabTest.RESULT_PENDING
-    ).values_list("barcode", flat=True)
-    response = requests.post(settings.MEDITECH_URL, data={"barcodes": barcodes})
-    response.raise_for_status()
-    results = response.json()["barcodes"]
-    for barcode, result in results.items():
-        if result != "pending":
-            try:
-                registration = SelfSwabTest.objects.get(barcode=barcode)
-            except (ValueError):
-                return
-
-            registration.result = result
-            registration.save()
-            rapidpro.create_flow_start(
-                urns=f"whatsapp:{registration.msisdn}",
-                flow=settings.SELFSWAB_RAPIDPRO_FLOW,
-                extra={
-                    "result": registration.result,
-                    "updated_at": registration.updated_at.strftime("%d/%m/%Y"),
-                },
+            barcodes = list(
+                SelfSwabTest.objects.filter(
+                    result=SelfSwabTest.RESULT_PENDING
+                ).values_list("barcode", flat=True)
             )
+            response = requests.post(
+                url=settings.MEDITECH_URL,
+                headers={"Content-Type": "application/json"},
+                json={"barcodes": barcodes},
+            )
+            response.raise_for_status()
+            results = response.json()["barcodes"]
+            for barcode, result in results.items():
+                if result != "pending":
+                    try:
+                        registration = SelfSwabTest.objects.get(barcode=barcode)
+                    except (ObjectDoesNotExist):
+                        continue
+
+                    if result == "":
+                        result = "Pending"
+                    registration.result = result
+                    rapidpro.create_flow_start(
+                        urns=f"whatsapp:{registration.msisdn}",
+                        flow=settings.SELFSWAB_RAPIDPRO_FLOW,
+                        extra={
+                            "result": registration.result,
+                            "updated_at": registration.updated_at.strftime("%d/%m/%Y"),
+                        },
+                    )
+                    registration.save()
 
     return "Finished syncing test results to Rapidpro"
--- selfswab/tests/test_tasks.py
+++ selfswab/tests/test_tasks.py
@@ -44,7 +44,7 @@ class PollMeditechForResults(TestCase):
                 "id": "3d9dc41c-8c18-4e3f-8afc-8970b1cae7c1",
                 "contact_id": "9e12d04c-af25-40b6-aa4f-57c72e8e3f91",
                 "msisdn": "27856454612",
-                "result": "",
+                "result": "Pending",
                 "barcode": "12345678",
                 "timestamp": self.updated_at,
             }
@@ -54,8 +54,18 @@ class PollMeditechForResults(TestCase):
                 "id": "9a4c5a43-9a48-44b0-ad32-8561217461c1",
                 "contact_id": "9e12d7hj-af25-40b6-bb4f-57c72c3c3f91",
                 "msisdn": "27895671234",
-                "result": "",
+                "result": "Pending",
                 "barcode": "87654321",
+                "timestamp": self.updated_at,
+            }
+        )
+        SelfSwabTest.objects.create(
+            **{
+                "id": "2da90072-f23c-4690-b1d0-e89aaddb982c",
+                "contact_id": "9e12d04c-af25-40b6-aa4f-57c72e8e3f91",
+                "msisdn": "27890001234",
+                "result": "Positive",
+                "barcode": "12341234",
                 "timestamp": self.updated_at,
             }
         )
@@ -75,15 +85,19 @@ class PollMeditechForResults(TestCase):
         poll_meditech_api_for_results()
 
         [call1, call2, call3] = responses.calls
+
         body3 = json.loads(call3.request.body)
         body2 = json.loads(call2.request.body)
+        body1 = json.loads(call1.request.body)
+
+        self.assertEqual(body1, {"barcodes": ["12345678", "87654321"]})
         self.assertEqual(
             body2,
             {
                 "flow": "321",
                 "urns": "whatsapp:27856454612",
                 "extra": {
-                    "result": "",
+                    "result": "Pending",
                     "updated_at": self.updated_at.strftime("%d/%m/%Y"),
                 },
             },
@@ -99,3 +113,52 @@ class PollMeditechForResults(TestCase):
                 },
             },
         )
+
+    @responses.activate
+    @override_settings(
+        RAPIDPRO_URL="https://rp-test.com",
+        MEDITECH_URL="https://medi-test.com",
+        SELFSWAB_RAPIDPRO_TOKEN="123",
+        SELFSWAB_RAPIDPRO_FLOW="321",
+    )
+    def test_poll_to_meditch_barcode_does_not_exist(self):
+        """
+        Should try get results from api with barcodes that
+        dont exist, assert that no api calls are made
+        """
+        SelfSwabTest.objects.create(
+            **{
+                "id": "3d9dc41c-8c18-4e3f-8afc-8970b1cae7c1",
+                "contact_id": "9e12d04c-af25-40b6-aa4f-57c72e8e3f91",
+                "msisdn": "27856454612",
+                "result": "",
+                "barcode": "12345678",
+                "timestamp": self.updated_at,
+            }
+        )
+        SelfSwabTest.objects.create(
+            **{
+                "id": "9a4c5a43-9a48-44b0-ad32-8561217461c1",
+                "contact_id": "9e12d7hj-af25-40b6-bb4f-57c72c3c3f91",
+                "msisdn": "27895671234",
+                "result": "",
+                "barcode": "87654321",
+                "timestamp": self.updated_at,
+            }
+        )
+
+        responses.add(
+            responses.POST,
+            "https://medi-test.com",
+            json={"barcodes": {"12121212": "", "88888888": "Negative"}},
+        )
+
+        responses.add(
+            responses.POST,
+            "https://rp-test.com/api/v2/flow_starts.json",
+            json=self.flow_response,
+        )
+
+        poll_meditech_api_for_results()
+
+        self.assertEqual(len(responses.calls), 1)
