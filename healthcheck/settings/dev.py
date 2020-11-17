3cc998d7b3e399cee36aba57a29632ea17663429
Codie Roelf <codiebeulaine@gmail.com>
2020-11-17 12:59:05 +0200

    add default env for allowed hosts

--- healthcheck/settings/dev.py
+++ healthcheck/settings/dev.py
@@ -1,8 +1,8 @@
 from healthcheck.settings.base import *  # noqa: F403
 
-SECRET_KEY = env.str("SECRET_KEY")  # noqa: F405
+SECRET_KEY = env.str("SECRET_KEY", "testsecretkey")  # noqa: F405
 DEBUG = env.bool("DEBUG", True)  # noqa: F405
 DATABASES = {
     "default": env.db(),  # noqa: F405
 }
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")  # noqa: F405
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default="*")  # noqa: F405
