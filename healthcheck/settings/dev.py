c0b337beff26ef0b484cda61c1c30300046ad156
Codie Roelf <codiebeulaine@gmail.com>
2020-11-17 14:48:45 +0200

    add migration

--- healthcheck/settings/dev.py
+++ healthcheck/settings/dev.py
@@ -1,8 +1,9 @@
 from healthcheck.settings.base import *  # noqa: F403
+import dj_database_url
 
 SECRET_KEY = env.str("SECRET_KEY", "testsecretkey")  # noqa: F405
 DEBUG = env.bool("DEBUG", True)  # noqa: F405
 DATABASES = {
-    "default": env.db(),  # noqa: F405
+    "default": dj_database_url.config("postgres://postgres:postgres@localhost:5432/healthcheck")
 }
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default="*")  # noqa: F405
