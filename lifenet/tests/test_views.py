c52e5aa6c8fccf47bda961a785229383d114fb3d
Codie Roelf <codiebeulaine@gmail.com>
2020-11-17 12:08:31 +0200

    udpate tests

--- lifenet/tests/test_views.py
+++ lifenet/tests/test_views.py
@@ -4,130 +4,90 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from tbconnect.models import TBCheck, TBTest
+from lifenet.models import LNCheck
 from userprofile.models import HealthCheckUserProfile
 from userprofile.tests.test_views import BaseEventTestCase
 
 
-class TBCheckViewSetTests(APITestCase, BaseEventTestCase):
-    url = reverse("tbcheck-list")
+class LNCheckViewSetTests(APITestCase, BaseEventTestCase):
+    url = reverse("lncheck-list")
 
     def test_data_validation(self):
         """
         The supplied data must be validated, and any errors returned
         """
         user = get_user_model().objects.create_user("test")
-        user.user_permissions.add(Permission.objects.get(codename="add_tbcheck"))
+        user.user_permissions.add(Permission.objects.get(codename="add_lncheck"))
         self.client.force_authenticate(user)
         response = self.client.post(self.url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_successful_request(self):
         """
-        Should create a new TBCheck object in the database
+        Should create a new LNCheck object in the database
         """
         user = get_user_model().objects.create_user("test")
-        user.user_permissions.add(Permission.objects.get(codename="add_tbcheck"))
+        user.user_permissions.add(Permission.objects.get(codename="add_lncheck"))
         self.client.force_authenticate(user)
         response = self.client.post(
             self.url,
             {
                 "msisdn": "27856454612",
                 "source": "USSD",
-                "province": "ZA-WC",
-                "city": "Cape Town",
-                "age": TBCheck.AGE_18T40,
-                "gender": TBCheck.GENDER_FEMALE,
-                "cough": True,
-                "fever": True,
-                "sweat": False,
-                "weight": True,
+                "age": LNCheck.AGE_18T39,
+                "cough": False,
+                "fever": False,
+                "sore_throat": False,
+                "difficulty_breathing": False,
+                "tracing": True,
+                "muscle_pain": True,
+                "smell": True,
                 "exposure": "yes",
                 "tracing": True,
-                "risk": TBCheck.RISK_LOW,
-                "location": "+40.20361+40.20361",
-                "follow_up_optin": True,
+                "risk": LNCheck.RISK_LOW,
                 "language": "eng",
             },
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        [tbcheck] = TBCheck.objects.all()
-        self.assertEqual(tbcheck.msisdn, "27856454612")
-        self.assertEqual(tbcheck.source, "USSD")
-        self.assertEqual(tbcheck.province, "ZA-WC")
-        self.assertEqual(tbcheck.city, "Cape Town")
-        self.assertEqual(tbcheck.age, TBCheck.AGE_18T40)
-        self.assertEqual(tbcheck.gender, TBCheck.GENDER_FEMALE)
-        self.assertTrue(tbcheck.cough)
-        self.assertTrue(tbcheck.fever)
-        self.assertFalse(tbcheck.sweat)
-        self.assertTrue(tbcheck.weight)
-        self.assertEqual(tbcheck.exposure, "yes")
-        self.assertTrue(tbcheck.tracing)
-        self.assertEqual(tbcheck.risk, TBCheck.RISK_LOW)
-        self.assertEqual(tbcheck.location, "+40.20361+40.20361")
-        self.assertTrue(tbcheck.follow_up_optin)
-        self.assertEqual(tbcheck.language, "eng")
+        [lncheck] = LNCheck.objects.all()
+        self.assertEqual(lncheck.msisdn, "27856454612")
+        self.assertEqual(lncheck.source, "USSD")
+        self.assertEqual(lncheck.age, LNCheck.AGE_18T39)
+        self.assertTrue(lncheck.cough)
+        self.assertTrue(lncheck.fever)
+        self.assertFalse(lncheck.sweat)
+        self.assertTrue(lncheck.weight)
+        self.assertEqual(lncheck.exposure, "yes")
+        self.assertTrue(lncheck.tracing)
+        self.assertEqual(lncheck.risk, LNCheck.RISK_LOW)
+        self.assertEqual(lncheck.language, "eng")
 
-    def test_location_validation(self):
+    def test_creates_user_profile(self):
         """
-        Should create a new TBCheck object in the database
+        The user profile should be created when the LN Check is saved
         """
         user = get_user_model().objects.create_user("test")
-        user.user_permissions.add(Permission.objects.get(codename="add_tbcheck"))
+        user.user_permissions.add(Permission.objects.get(codename="add_lncheck"))
         self.client.force_authenticate(user)
         response = self.client.post(
             self.url,
             {
                 "msisdn": "27856454612",
                 "source": "USSD",
-                "province": "ZA-WC",
-                "city": "Cape Town",
-                "age": TBCheck.AGE_18T40,
-                "gender": TBCheck.GENDER_FEMALE,
-                "cough": True,
-                "fever": True,
-                "sweat": False,
-                "weight": True,
+                "age": LNCheck.AGE_18T39,
+                "cough": False,
+                "fever": False,
+                "sore_throat": False,
+                "difficulty_breathing": False,
+                "tracing": True,
+                "muscle_pain": True,
+                "smell": True,
                 "exposure": "yes",
                 "tracing": True,
-                "risk": TBCheck.RISK_LOW,
-            },
-        )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            response.json(),
-            {"non_field_errors": ["location and city_location are both None"]},
-        )
-
-    def test_creates_user_profile(self):
-        """
-        The user profile should be created when the TB Check is saved
-        """
-        user = get_user_model().objects.create_user("test")
-        user.user_permissions.add(Permission.objects.get(codename="add_tbcheck"))
-        self.client.force_authenticate(user)
-        response = self.client.post(
-            self.url,
-            {
-                "msisdn": "+27856454612",
-                "source": "USSD",
-                "province": "ZA-WC",
-                "city": "Cape Town",
-                "age": TBCheck.AGE_18T40,
-                "gender": TBCheck.GENDER_FEMALE,
-                "cough": True,
-                "fever": True,
-                "sweat": False,
-                "weight": True,
-                "exposure": "yes",
-                "tracing": True,
-                "risk": TBCheck.RISK_LOW,
-                "location": "+40.20361+40.20361",
+                "risk": LNCheck.RISK_LOW,
+                "language": "eng",
             },
             format="json",
         )
         profile = HealthCheckUserProfile.objects.get(msisdn="+27856454612")
-        self.assertEqual(profile.province, "ZA-WC")
-        self.assertEqual(profile.city, "Cape Town")
-        self.assertEqual(profile.age, TBCheck.AGE_18T40)
+        self.assertEqual(profile.age, LNCheck.AGE_18T39)
