03a56389ba1fb6ca313ba385fe2fc725504f9b6f
Codie Roelf <codiebeulaine@gmail.com>
2020-11-18 10:44:30 +0200

    flake8

--- healthcheck/settings/dev.py
+++ healthcheck/settings/dev.py
@@ -4,6 +4,7 @@ import dj_database_url
 SECRET_KEY = env.str("SECRET_KEY", "testsecretkey")  # noqa: F405
 DEBUG = env.bool("DEBUG", True)  # noqa: F405
 DATABASES = {
-    "default": dj_database_url.config("postgres://postgres:postgres@localhost:5432/healthcheck")
+    "default": dj_database_url.config(
+        "postgres://postgres:postgres@localhost:5432/healthcheck")
 }
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default="*")  # noqa: F405
