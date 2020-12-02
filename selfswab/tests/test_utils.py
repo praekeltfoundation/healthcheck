4fe3c69a7749cbbbf6af29aacbedd7bb03c71f46
OMosimege <onalerona.mosimege@gmail.com>
2020-12-02 14:55:55 +0200

    changed tests for the barcode verification

--- selfswab/tests/test_utils.py
+++ selfswab/tests/test_utils.py
@@ -7,17 +7,17 @@ class IsBarcodeFormatValidTests(TestCase):
     def test_is_barcode_format_valid(self):
         self.assertFalse(is_barcode_format_valid("123"))
         self.assertFalse(is_barcode_format_valid("CP123"))
-        self.assertFalse(is_barcode_format_valid("CP159600000"))
-        self.assertFalse(is_barcode_format_valid("CP159600101"))
-        self.assertFalse(is_barcode_format_valid("CP158600001"))
+        self.assertTrue(is_barcode_format_valid("CP159600000"))
+        self.assertTrue(is_barcode_format_valid("CP159600101"))
+        self.assertTrue(is_barcode_format_valid("CP158600001"))
 
         self.assertTrue(is_barcode_format_valid("CP159600001"))
         self.assertTrue(is_barcode_format_valid("CP159600055"))
         self.assertTrue(is_barcode_format_valid("CP159600100"))
 
-        self.assertFalse(is_barcode_format_valid("CP999T99000"))
-        self.assertFalse(is_barcode_format_valid("CP999T99101"))
-        self.assertFalse(is_barcode_format_valid("CP998T99001"))
+        self.assertTrue(is_barcode_format_valid("CP999T99000"))
+        self.assertTrue(is_barcode_format_valid("CP999T99101"))
+        self.assertTrue(is_barcode_format_valid("CP998T99001"))
 
         self.assertTrue(is_barcode_format_valid("CP999T99001"))
         self.assertTrue(is_barcode_format_valid("CP999T99055"))
