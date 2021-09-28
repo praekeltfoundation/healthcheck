c785bcca4f9cc8906845a8e18a55ea616d7e7654
devchima <developer.chima@gmail.com>
2021-09-28 03:20:28 -0400

    Skip location and city_location if age is <18

---
+++ tbconnect/test_serializers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from tbconnect.serializers import TBCheckSerializer
+
+class TBCheckSerializerTests(TestCase):
+    def test_valid_tbcheck(self):
+        """
+        If age is <18 skip location and location_
+        """
+        data = {
+            "msisdn": "+2349039756628",
+            "source": "WhatsApp",
+            "province": "ZA-GT",
+            "city": "<not collected>",
+            "age": "<18",
+            "gender":"male",
+            "cough": "True",
+            "fever": "False",
+            "sweat": "False",
+            "weight": "False",
+            "exposure": "no",
+            "tracing": "False",
+            "risk": "low"
+        }
+        serializer = TBCheckSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(
+            dict(serializer.validated_data),
+            {
+                'age': '<18',
+                'city': '<not collected>',
+                'cough': True,
+                'exposure': 'no',
+                'fever': False,
+                'gender': 'male',
+                'msisdn': '+2349039756628',
+                'province': 'ZA-GT',
+                'risk': 'low',
+                'source': 'WhatsApp',
+                'sweat': False,
+                'tracing': False,
+                'weight': False
+            }
+        )
