1394a30597aeb936f2ff5a59b2bbe974d44c69bf
devchima <developer.chima@gmail.com>
2021-09-28 03:46:08 -0400

    Reformat black

--- tbconnect/test_serializers.py
+++ tbconnect/test_serializers.py
@@ -20,25 +20,25 @@ class TBCheckSerializerTests(TestCase):
             "weight": "False",
             "exposure": "no",
             "tracing": "False",
-            "risk": "low"
+            "risk": "low",
         }
         serializer = TBCheckSerializer(data=data)
         self.assertTrue(serializer.is_valid())
         self.assertEqual(
             dict(serializer.validated_data),
             {
-                'age': '<18',
-                'city': '<not collected>',
-                'cough': True,
-                'exposure': 'no',
-                'fever': False,
-                'gender': 'male',
-                'msisdn': '+2349039756628',
-                'province': 'ZA-GT',
-                'risk': 'low',
-                'source': 'WhatsApp',
-                'sweat': False,
-                'tracing': False,
-                'weight': False
-            }
+                "age": "<18",
+                "city": "<not collected>",
+                "cough": True,
+                "exposure": "no",
+                "fever": False,
+                "gender": "male",
+                "msisdn": "+2349039756628",
+                "province": "ZA-GT",
+                "risk": "low",
+                "source": "WhatsApp",
+                "sweat": False,
+                "tracing": False,
+                "weight": False,
+            },
         )
