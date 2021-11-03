567dd257251952e049eb01895795b31ecdd412e3
Rudi Giesler <rudi@praekelt.org>
2021-11-03 17:21:54 +0200

    Submit vaxchamps result to RapidPro

---
+++ vaxchamps/test_views.py
@@ -0,0 +1,80 @@
+import json
+
+import responses
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from healthcheck.settings.base import VAXCHAMPS_RAPIDPRO_FLOW
+
+
+class RegistrationViewSetTests(APITestCase):
+    def setUp(self):
+        responses.add(
+            responses.POST,
+            "https://whatsapp.turn.io/v1/contacts",
+            json={"contacts": [{"status": "valid"}]},
+        )
+        responses.add(
+            responses.POST,
+            "https://textit.in/api/v2/flow_starts.json",
+            json={
+                "uuid": "09d23a05-47fe-11e4-bfe9-b8f6b119e9ab",
+                "flow": {
+                    "uuid": "f5901b62-ba76-4003-9c62-72fdacc1b7b7",
+                    "name": "Thrift Shop",
+                },
+                "groups": [],
+                "contacts": [],
+                "restart_participants": True,
+                "extra": {},
+                "status": "complete",
+                "created_on": "2013-08-19T19:11:21.082Z",
+                "modified_on": "2013-08-19T19:11:21.082Z",
+            },
+        )
+        user = get_user_model().objects.create_user("test")
+        user.user_permissions.add(
+            Permission.objects.get(codename="create_registration")
+        )
+        self.client.force_authenticate(user)
+
+    @responses.activate
+    def test_valid_registration(self):
+        url = reverse("registrations-list")
+        data = {
+            "name": "test name",
+            "cell_no": "0820001001",
+            "lang": 1,
+            "comms_choice": 1,
+            "email": "test@example.org",
+            "popia_consent": 0,
+            "province": 9,
+            "district": 49,
+            "gender": 2,
+            "age": 3,
+        }
+        with self.settings(
+            RAPIDPRO_URL="https://textit.in",
+            VAXCHAMPS_RAPIDPRO_FLOW="fde8a8e4-b973-42e4-aa96-dbc5ef70164d",
+        ):
+            response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        [_, request] = responses.calls
+        self.assertEqual(
+            json.loads(request.request.body)["extra"],
+            {
+                "age": "18-34 years",
+                "cell_no": "+27820001001",
+                "comms_choice": 1,
+                "district": "City of Cape Town",
+                "email": "test@example.org",
+                "gender": "Male",
+                "lang": "eng",
+                "name": "test name",
+                "popia_consent": 0,
+                "province": "Western Cape",
+            },
+        )
--- vaxchamps/views.py
+++ vaxchamps/views.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import status, viewsets
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
+from temba_client.v2 import TembaClient
 
-from vaxchamps.serializers import RegistrationSerializer
+from vaxchamps.serializers import (
+    AGES,
+    DISTRICTS,
+    GENDERS,
+    LANGUAGES,
+    PROVINCES,
+    RegistrationSerializer,
+)
 
 
 class CreateRegistrationPermissions(DjangoModelPermissions):
@@ -16,7 +25,25 @@ class RegistrationViewSet(viewsets.ViewSet):
     permission_classes = [CreateRegistrationPermissions]
     queryset = get_user_model().objects.none()
 
+    def start_rapidpro_flow(self, data):
+        data = data.copy()
+        urn = f"whatsapp:{data['cell_no'].lstrip('+')}"
+        data["lang"] = LANGUAGES[data["lang"]]
+        if data.get("province"):
+            data["province"] = PROVINCES[data["province"]]["name"]
+        if data.get("district"):
+            data["district"] = DISTRICTS[data["district"]]
+        if data.get("gender"):
+            data["gender"] = GENDERS[data["gender"]]
+        if data.get("age"):
+            data["age"] = AGES[data["age"]]
+        rapidpro = TembaClient(settings.RAPIDPRO_URL, settings.RAPIDPRO_TOKEN)
+        rapidpro.create_flow_start(
+            settings.VAXCHAMPS_RAPIDPRO_FLOW, urns=[urn], extra=data
+        )
+
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
+        self.start_rapidpro_flow(serializer.data)
         return Response(serializer.data, status.HTTP_201_CREATED)
