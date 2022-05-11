b91ed7d003c4ec866c0b90be5d113a689a21d7e0
Buhle <buhle@praekelt.org>
2022-05-11 17:10:58 +0200

    Revert "Research consent field"
    This reverts commit 3c281d34537eb80079d848342f5617f86052075e.

--- tbconnect/models.py
+++ tbconnect/models.py
@@ -107,7 +107,6 @@ class TBCheck(ExportModelOperationsMixin("tb-check"), models.Model):
     commit_get_tested = models.CharField(
         max_length=3, choices=COMMIT_CHOICES, null=True, blank=True
     )
-    research_consent = models.BooleanField(null=True)
 
     @property
     def hashed_msisdn(self):
