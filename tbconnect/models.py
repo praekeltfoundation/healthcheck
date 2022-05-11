a2cf6aefdc967aa6d0aa3952fd8da101b9921371
Buhle <buhle@praekelt.org>
2022-05-11 12:08:33 +0200

    added research consent

--- tbconnect/models.py
+++ tbconnect/models.py
@@ -107,6 +107,7 @@ class TBCheck(ExportModelOperationsMixin("tb-check"), models.Model):
     commit_get_tested = models.CharField(
         max_length=3, choices=COMMIT_CHOICES, null=True, blank=True
     )
+    research_consent = models.BooleanField(default=False)
 
     @property
     def hashed_msisdn(self):
