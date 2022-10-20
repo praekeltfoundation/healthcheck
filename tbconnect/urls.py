4cbb0fd11e41e12e28733c7e565a141bb1aa3fed
Bearnard Hibbins <bearnard@praekelt.org>
2022-10-20 16:03:10 +0200

    Add reset api.

--- tbconnect/urls.py
+++ tbconnect/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 
-from tbconnect.views import TBCheckViewSet, TBTestViewSet
+from tbconnect.views import TBCheckViewSet, TBResetViewSet, TBTestViewSet
 
 v2router = routers.DefaultRouter()
 v2router.register("tbcheck", TBCheckViewSet)
 v2router.register("tbtest", TBTestViewSet)
+v2router.register("tbreset", TBResetViewSet, basename="tbreset")
