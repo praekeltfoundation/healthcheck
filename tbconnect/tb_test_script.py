8f6cf87bb32400dd53b5463466dccded8cce38ac
Buhle <buhle@praekelt.org>
2023-02-02 09:34:16 +0200

    added order_by latest for btbcheck

--- tbconnect/tb_test_script.py
+++ tbconnect/tb_test_script.py
@@ -6,11 +6,16 @@ def run():
     tbtests = TBTest.objects.all()
 
     for tbtest in tbtests:
+        print(tbtest.source)
         # Get recent tbcheck to update tbtest source
-        tbcheck = TBCheck.objects.filter(
-            msisdn=tbtest.msisdn, completed_timestamp__lt=tbtest.timestamp
-        ).first()
-
+        tbcheck = (
+            TBCheck.objects.filter(
+                msisdn=tbtest.msisdn, completed_timestamp__lt=tbtest.timestamp
+            )
+            .order_by("-completed_timestamp")
+            .first()
+        )
+        print("Tbcheck", tbcheck.source)
         if tbcheck.source == "USSD" and tbtest.source != "SMS":
             # Update mismatching test to sms
             tbtest.source = "SMS"
