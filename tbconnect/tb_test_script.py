537e434dbb9195574f49909e9d2426dd02c7461c
Buhle <buhle@praekelt.org>
2023-02-02 09:44:45 +0200

    Removed print statements

--- tbconnect/tb_test_script.py
+++ tbconnect/tb_test_script.py
@@ -6,7 +6,6 @@ def run():
     tbtests = TBTest.objects.all()
 
     for tbtest in tbtests:
-        print(tbtest.source)
         # Get recent tbcheck to update tbtest source
         tbcheck = (
             TBCheck.objects.filter(
@@ -15,7 +14,7 @@ def run():
             .order_by("-completed_timestamp")
             .first()
         )
-        print("Tbcheck", tbcheck.source)
+
         if tbcheck.source == "USSD" and tbtest.source != "SMS":
             # Update mismatching test to sms
             tbtest.source = "SMS"
