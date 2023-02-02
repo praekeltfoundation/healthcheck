1361b4d4a9556e866dad272c6e82e05ae9f6ef08
Buhle <buhle@praekelt.org>
2023-02-02 09:13:55 +0200

    reformat script

--- tbconnect/tb_test_script.py
+++ tbconnect/tb_test_script.py
@@ -7,7 +7,9 @@ def run():
 
     for tbtest in tbtests:
         # Get recent tbcheck to update tbtest source
-        tbcheck = TBCheck.objects.filter(msisdn=tbtest.msisdn, completed_timestamp__lt=tbtest.timestamp).first()
+        tbcheck = TBCheck.objects.filter(
+            msisdn=tbtest.msisdn, completed_timestamp__lt=tbtest.timestamp
+        ).first()
 
         if tbcheck.source == "USSD" and tbtest.source != "SMS":
             # Update mismatching test to sms
