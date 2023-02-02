e18e5d187b6020476827d86bd5daf1f3b2d6be83
Buhle <buhle@praekelt.org>
2023-02-02 08:38:20 +0200

    Script to update tbtest source

---
+++ tbconnect/tb_test_script.py
@@ -0,0 +1,17 @@
+from .models import TBCheck, TBTest
+
+
+def run():
+    # Get all TB tests
+    tbtests = TBTest.objects.all()
+
+    for tbtest in tbtests:
+        # Get recent tbcheck to update tbtest source
+        tbcheck = TBCheck.objects.filter(msisdn=tbtest.msisdn, completed_timestamp__lt=tbtest.timestamp).first()
+
+        if tbcheck.source == "USSD" and tbtest.source != "SMS":
+            # Update mismatching test to sms
+            tbtest.source = "SMS"
+            tbtest.save()
+
+    print("Script Completed")
