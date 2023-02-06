b3512fd7f27ec6803835594a1eff600d103c3574
Buhle <buhle@praekelt.org>
2023-02-06 10:51:08 +0200

    Added print statement to test

--- tbconnect/tb_test_script.py
+++ tbconnect/tb_test_script.py
@@ -7,6 +7,9 @@ def run():
 
     for tbtest in tbtests:
         # Get recent tbcheck to update tbtest source
+        print(
+            "Contact: ", tbtest.msisdn
+        )  # To test in QA AttributeError: 'NoneType' object has no attribute 'source'
         tbcheck = (
             TBCheck.objects.filter(
                 msisdn=tbtest.msisdn, completed_timestamp__lt=tbtest.timestamp
@@ -14,7 +17,7 @@ def run():
             .order_by("-completed_timestamp")
             .first()
         )
-
+        print("TBcheck", tbcheck.msisdn, tbcheck.source)
         if tbcheck.source == "USSD" and tbtest.source != "SMS":
             # Update mismatching test to sms
             tbtest.source = "SMS"
