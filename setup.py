c5b9dec6a6bd477e1752269a7b35dba4374152af
buhle79 <buhle@praekelt.org>
2024-07-30 12:34:00 +0200

    numpy error fix

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Framework :: Django",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
